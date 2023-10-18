5db438a918b03c8737c18dc6f6d83a833bfc19e3
Piotr Krawiec <piotr.krawiec23@gmail.com>
2023-10-18 08:04:50 +0200

    tests: fix tests for parser

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,4 +1,4 @@
-from zipfs_law.parse import split, clean, count_tokens, histogram
+from zipfs_law.parse import split, clean, count_tokens, count_to_dataframe
 
 def test_split():
     """Test the split function."""
@@ -16,10 +16,10 @@ def test_count_tokens():
     tokens = ['this', 'is', 'a', 'test', 'this', 'is', 'another', 'test']
     assert count_tokens(tokens) == {'this': 2, 'is': 2, 'a': 1, 'test': 2, 'another': 1}
     
-def test_histogram():
-    """Test the histogram function"""
+def test_count_to_dataframe():
+    """Test the count_to_dataframe function"""
     counts = {'this': 2, 'is': 2, 'a': 1, 'test': 2, 'another': 1}
-    df = histogram(counts)
+    df = count_to_dataframe(counts)
     assert df.shape == (5, 2)
     assert df['token'].tolist() == ['this', 'is', 'test', 'a', 'another']
     assert df['count'].tolist() == [2, 2, 2, 1, 1]
